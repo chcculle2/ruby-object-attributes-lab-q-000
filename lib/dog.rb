28f3999a316dbad303882a0df25fa85efd543b80
chcculle <cathy@softwareoptions.com>
2016-04-10 07:47:50 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def initialize(name="Fido", breed="GSD")
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+end
